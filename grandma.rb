e32dd9077021c17c4241d350bf6cb452e330cdbd
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-09-19 03:21:32 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,14 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase.upcase
-    puts "NO NOT SINCE 1938"
+if phrase == "I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
+elsif phrase == "WHAT DID YOU EAT TODAY?"
+  "NO, NOT SINCE 1938!"
+elsif phrase == "WHAT?"
+  "NO, NOT SINCE 1938!"
+else phrase == "Hi!"
+  "HUH?! SPEAK UP, SONNY!"
+end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
