909ece25d77c39673102fa045dc3e4c5aca7e135
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-09-19 03:28:45 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-if phrase == phrase.upcase
+if phrase == "I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
+elsif phrase == phrase.upcase
   "NO, NOT SINCE 1938!"
 else
   "HUH?! SPEAK UP, SONNY!"
